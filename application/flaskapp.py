4b5e9f6e75691ab9727c2ba1a51661ee525e7ec3
sriramsv <sriramsv1991@gmail.com>
2016-11-27 00:24:13 -0500

    Working Day Query

--- application/flaskapp.py
+++ application/flaskapp.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 api = Api(app)
 class HelloWorld(Resource):
     def get(self):
-        return "contextify Backend API"
+        return "Contextify Backend API"
 
 class UserData(Resource):
 	def get(self,username):
@@ -18,9 +18,15 @@ class UserData(Resource):
 		res=util.gettimeresp(user.get_states())
 		return jsonify(res)
 
+class UserDatabyDay(Resource):
+	def get(self,username,day):
+		user=models.User(username)
+		res=user.get_states_by_day(day)
+		return jsonify(res)
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(UserData,"/user/<string:username>")
+api.add_resource(UserDatabyDay,"/user/<string:username>/<string:day>")
 
 if __name__=="__main__":
 	app.run(host="0.0.0.0",port=4000)
--- application/models.py
+++ application/models.py
@@ -1,29 +1,35 @@
 import dbtest
 import util
+from pymongo import MongoClient,errors
 
 class User():
     def __init__(self,name):
         self.name=name.title()
         self.days={1:"Sunday",2:"Monday",3:"Tuesday",4:"Wednesday",5:"Thursday",6:"Friday",7:"Saturday"}
+        self.dbclient = MongoClient('localhost', 27017)
+        self.db=self.dbclient.contextify
 
     def __repr__(self):
         return self.name
 
     def get_states(self):
-    	loc=dbtest.db.location
+    	loc=self.db.location
     	res=loc.find({"User":self.name})
         return list(res)
 
     def get_states_by_day(self,day=None):
+        if day:
+            day=day.title()
     	days={"Sunday":1,"Monday":2,"Tuesday":3,"Wednesday":4,"Thursday":5,"Friday":6,"Saturday":7}
-    	loc=db.location
+    	loc=self.db.location
     	s=days[day]
     	pipe=[
     	{
     	    "$project": {
+                "_id":0,
+                "User":"$User",
     	        "dow": { "$dayOfWeek": "$Startdate" },
     	        "State":"$State",
-    	        "User":"$User",
     	        "Start":"$Start",
     	        "End":"$End",
     	    }
@@ -33,4 +39,6 @@ class User():
     	}
     	]
     	res=loc.aggregate(pipeline=pipe)
-    	return res["result"]
+
+        print list(res)
+        return res["result"]
